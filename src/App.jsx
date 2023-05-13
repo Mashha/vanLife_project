9abfb0cd663c3a602e73d03565bfceaf7db81e60
Masa Orel <orel.masha89@gmail.com>
2023-05-13 16:22:53 +0200

    added outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
-import Nav from "./components/Nav";
 import Vans from "./components/Vans";
 import VanDetail from "./components/VanDetail";
 import "./App.css";
 import "./server";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
