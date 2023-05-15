771768cbdbf77f60d6b3c905ca5bd9a399d78402
Masa Orel <orel.masha89@gmail.com>
2023-05-15 15:59:20 +0200

    added host van details

--- src/components/host/HostVanDetail.jsx
+++ src/components/host/HostVanDetail.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function HostVanDetail() {
-  return <div>HostVanDetail</div>;
+  const params = useParams();
+  const [currentVan, setVanDetail] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVanDetail(data.vans));
+  }, [params.id]);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HostVanDetail;
