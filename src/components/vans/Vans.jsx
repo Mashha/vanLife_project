52496ea7e39158cc3a3f90b9c0ec9bb4ebbed731
Masa Orel <orel.masha89@gmail.com>
2023-05-18 10:31:23 +0200

    added search params

--- src/components/vans/Vans.jsx
+++ src/components/vans/Vans.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans() {
   const [vansData, setVansData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
 
   useEffect(() => {
     fetch("/api/vans")
